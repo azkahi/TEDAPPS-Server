2e543db297f749d82cef3afdb15dd62974cfc9ae
Azka Hanif <azka_h_imtiyaz@telkomsel.co.id>
2019-12-02 18:41:23 +0700

    Fix type in user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -46,6 +46,6 @@ class UsersController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def user_params
-      params.require(:user).permit(:name, :phone, :email, :type, :expertise)
+      params.require(:user).permit(:name, :phone, :email, :user_type, :expertise)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_02_090820) do
+ActiveRecord::Schema.define(version: 2019_12_02_113546) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 2019_12_02_090820) do
     t.string "name"
     t.string "phone"
     t.string "email"
-    t.string "type"
+    t.string "user_type"
     t.string "expertise"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
