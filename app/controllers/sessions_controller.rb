866c23d737357b81e57c534e931ade489657dcf3
Azka Hanif <azka_h_imtiyaz@telkomsel.co.id>
2019-12-11 00:10:29 +0700

    add get individual session detail

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -71,6 +71,42 @@ class SessionsController < ApplicationController
     render plain: top_sessions_csv.to_s
   end
 
+  def get_session_detail
+    @user = User.all
+
+    top_sessions = []
+
+    @user.each { |user|
+      @sessions = user.sessions
+
+      attended_flags = [0,0,0,0,0,0,0,0,0,0,0]
+
+      @sessions.each do |session|
+        puts 'test'
+        puts session.key
+        if Integer(session.key) >= 0 and Integer(session.key) <= 10
+          attended_flags[session.key.to_i] = 1
+        end
+      end
+
+      top_session = { :name => user.name, :phone => user.phone, :email => user.email, :user_type => user.user_type, :class_0 => attended_flags[0], :class_1 => attended_flags[1], :class_2 => attended_flags[2], :class_3 => attended_flags[3], :class_4 => attended_flags[4], :class_5 => attended_flags[5], :class_6 => attended_flags[6], :class_7 => attended_flags[7], :class_8 => attended_flags[8], :class_9 => attended_flags[9], :class_10 => attended_flags[10], }
+
+      top_sessions << top_session
+    }
+
+    attributes = %w{name phone email user_type class_0 class_1 class_2 class_3 class_4 class_5 class_6 class_7 class_8 class_9 class_10}
+
+    top_sessions_csv = CSV.generate(headers: true) do |csv|
+      csv << attributes
+
+      top_sessions.each do |top_session|
+        csv << [ top_session[:name], top_session[:phone], top_session[:email], top_session[:user_type], top_session[:class_0], top_session[:class_1], top_session[:class_2], top_session[:class_3], top_session[:class_4], top_session[:class_5], top_session[:class_6], top_session[:class_7], top_session[:class_8], top_session[:class_9], top_session[:class_10] ]
+      end
+    end
+
+    render plain: top_sessions_csv.to_s
+  end
+
   def normalize_value
     @users = User.all
 
--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,6 @@ Rails.application.routes.draw do
   post 'end_quiz', to: 'scores#end_quiz'
   post 'show_score', to: 'scores#show_score'
   get 'session_report', to: 'sessions#get_top'
+  get 'session_report_detail', to: 'sessions#get_session_detail'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
